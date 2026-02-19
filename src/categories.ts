a71497efce6e05805b25028dcfe74a92bc28956d
Victor <victor.chai@feedme.cc>
2026-02-20 01:05:34 +0800

    feat: populate type when get categories

--- src/categories.ts
+++ src/categories.ts
@@ -1,7 +1,16 @@
 export default async (request: Request, url: URL, env: Cloudflare.Env) => {
   if (request.method === "GET") {
+    const { results } = await env.DB.prepare("SELECT * FROM types").all();
+    const types = results.reduce((acc, type) => {
+      acc[type.id as number] = type.name;
+      return acc;
+    }, {} as Record<number, string>)
+
     const resp = await env.DB.prepare("SELECT * FROM categories").all();
-    return new Response(JSON.stringify(resp.results), {
+    return new Response(JSON.stringify(resp.results.map((v) => ({
+      ...v,
+      pf_type: types[v.type_id as number]
+    }))), {
       status: 200,
       headers: {
         "content-type": "application/json",
